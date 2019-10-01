df60d9f50abd3c16ac603b58e370fde815392940
Anne Chen <annewchen@gmail.com>
2019-09-30 23:54:23 -0700

    Initial set up

---
+++ VaroTheMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VaroTheMovieDB
+//
+//  Created by Anne Chen on 9/30/19.
+//  Copyright © 2019 Anne Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
